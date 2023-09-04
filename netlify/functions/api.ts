e6b9793857cb4f85e21498e9bffffae9063856c9
lldan <safinildan@gmail.com>
2023-09-04 21:58:31 +0300

    _24

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,10 +1,22 @@
 import express, { Router } from 'express';
 import serverless from 'serverless-http';
-
+import { otheractualseason } from '../../src/api';
+import { responseType } from '../../src/@types/response';
 const api = express();
 
 const router = Router();
-router.get('/hello', (req, res) => res.send({ text: 'Hello World!' }));
+router.get('/other/actualseason', async (req, res) => {
+  try {
+    const response = await otheractualseason();
+    res.send(response);
+  } catch (error) {
+    const response: responseType = {
+      OK: false,
+      error,
+    };
+    res.status(500).send(response);
+  }
+});
 
 api.use('/api/', router);
 
